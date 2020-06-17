c057cb2e642d167f7d7e6a943111e049f38542ce
Krys <krysngabi@gmail.com>
2020-06-16 23:27:37 -0700

    Initial Commit

---
+++ BisoSound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BisoSound
+//
+//  Created by Krys Ngabi on 6/16/20.
+//  Copyright © 2020 Krys Ngabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
